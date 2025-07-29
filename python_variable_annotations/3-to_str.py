c83e9b6b79977830b05bfb579dff48b5f4aca0b1
User10538 <10538@holbertonstudents.com>
2025-07-29 15:24:03 +1000

    Fix pycodestyle

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
 This module provides a type-annotated function to_str that
- takes a float n as argument and returns the string representation of the float.
+ takes a float n as argument and returns
+ the string representation of the float.
 """
 
 
