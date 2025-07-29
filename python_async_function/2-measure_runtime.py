dba8f4ad72ab212144a1b1286ba3fde5929c0457
User10538 <10538@holbertonstudents.com>
2025-07-30 00:09:41 +1000

    Fix error

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Create a measure_time function with integers n 
+Create a measure_time function with integers n
 and max_delay as arguments that measures the total
  execution time for wait_n(n, max_delay), and returns
  total_time / n.
@@ -16,7 +16,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 def measure_time(n: int, max_delay: int = 0) -> float:
     """
     Create a measure_time function with integers n and max_delay as
-     arguments that measures the total execution time for 
+    arguments that measures the total execution time for
      wait_n(n, max_delay)
 
     Args:
