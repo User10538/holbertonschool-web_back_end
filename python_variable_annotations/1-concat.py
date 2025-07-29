54c798d9122a871c318fe2a420fd052d163b5e82
User10538 <10538@holbertonstudents.com>
2025-07-29 04:16:16 +0000

    Fix error

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -19,4 +19,4 @@ def concat(str1: str, str2: str) -> str:
         raise TypeError("str1 must be a string")
     if not isinstance(str2, str):
         raise TypeError("str2 must be a string")
-    return str1 + " " + str2
+    return str1 + str2
