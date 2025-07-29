ce13aaa574aaf1286b3464211d4ea3733c8d69af
User10538 <10538@holbertonstudents.com>
2025-07-29 21:17:27 +1000

    Fix error

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int = 0):
+async def wait_random(max_delay: int = 10) -> float:
     """
     asynchronous coroutine that takes in an integer argument
     (max_delay, with a default value of 10) named wait_random
