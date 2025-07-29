c1c4ddbadf635dc718ead3db377a829f062bdc04
User10538 <10538@holbertonstudents.com>
2025-07-29 01:51:43 +0000

    Fix pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
     if not isinstance(a, float):
-            raise TypeError("a must be float")
+        raise TypeError("a must be float")
     if not isinstance(b, float):
-            raise TypeError("b must be float")
-    
+        raise TypeError("b must be float")
     return a + b
