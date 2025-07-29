809d78a639d1ab8d7cba056fd39a1174e0215579
User10538 <10538@holbertonstudents.com>
2025-07-29 01:54:14 +0000

    Fix documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to add two numbers using type annotations.
+"""
+
 def add(a: float, b: float) -> float:
     if not isinstance(a, float):
         raise TypeError("a must be float")
