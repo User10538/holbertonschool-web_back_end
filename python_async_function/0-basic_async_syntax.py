89d4b89c60229dbb7bae5f0c172bd84fb972b7cb
User10538 <10538@holbertonstudents.com>
2025-07-29 21:21:52 +1000

    Fix error

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+This module provides asynchronous coroutine that takes in an integer argument
+ named wait_random that waits for a random delay between 0 and max_delay
+ seconds and eventually returns it.
+"""
+
 
 import random
 import asyncio
@@ -6,16 +12,14 @@ import asyncio
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    asynchronous coroutine that takes in an integer argument
-    (max_delay, with a default value of 10) named wait_random
+    integer argument max_delay, with a default value of 10 named wait_random
       that waits for a random delay between 0 and max_delay
 
     Args:
-    lst: list.
+    max_delay: int = 10
 
     Returns:
-    random delay between 0 and
-      max_delay (included and float value) seconds
+    float: The actual delay time used.
     """
 
     get_random = random.uniform(0, max_delay)
