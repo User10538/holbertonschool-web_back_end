ca3029a5c6a4acedd83cba264e2586ef17b729f7
User10538 <10538@holbertonstudents.com>
2025-07-29 04:29:21 +0000

    2. Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Basic annotations - floor
+
+    Args:
+    n: str: The float.
+
+    Returns:
+    The floor of the float.
+    """
+    if not isinstance(n, float):
+        raise TypeError("n must be a float")
+    
+    return int(n)
