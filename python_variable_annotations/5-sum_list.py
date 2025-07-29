712de01cd0b0224991bc57ef4e1472e16105c6de
User10538 <10538@holbertonstudents.com>
2025-07-29 16:11:38 +1000

    Fix error

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+from typing import List
+
 """
 This module provides a function sum_list which takes a list input_list of
  floats as argument and returns their sum as a float.
 """
 
 
-def sum_list (input_list : list[float]) -> float:
+def sum_list (input_list : List[float]) -> float:
     """
     Takes a list input_list of floats as argument
 
