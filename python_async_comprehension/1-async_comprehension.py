25bf8d7b424313411460ee1145bcd3d99e71eabf
User10538 <10538@holbertonstudents.com>
2025-07-30 17:34:50 +1000

    Fix error

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Import async_generator from the previous task and 
+Import async_generator from the previous task and
 then write a coroutine called
  async_comprehension that takes no arguments.
 """
@@ -8,9 +8,19 @@ then write a coroutine called
 
 from typing import List
 
-async_generator = __import__('0-async_generator').async_generator 
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """
+    Write a coroutine called async_generator
+     that takes no arguments.
+
+    Args:
+    none
+
+    Returns:
+    yield a random number between 0 and 10
+    """
     # iterate over values (x) yielded from the asynchronous generator.
     return [x async for x in async_generator()]
