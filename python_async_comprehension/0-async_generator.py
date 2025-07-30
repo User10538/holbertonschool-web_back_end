531ddddf05815ffd40eb7499079d57b380ef0a40
User10538 <10538@holbertonstudents.com>
2025-07-30 17:03:46 +1000

    Fix error

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,8 +2,10 @@
 
 import random
 import asyncio
+from typing import Generator
 
-async def async_generator():
+
+async def async_generator() -> Generator[float, None, None]:
 
     for x in range(10):
         await asyncio.sleep(1)
