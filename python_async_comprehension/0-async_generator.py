3d859e3ee6e195c0fc08665ce1c3159d8ea76420
User10538 <10538@holbertonstudents.com>
2025-07-30 18:07:35 +1000

    Fix error

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -23,6 +23,6 @@ async def async_generator() -> Generator[float, None, None]:
     Generator
     """
 
-    for x in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,15 +9,15 @@ Import async_comprehension from the previous file
 import asyncio
 import time
 
-async_comprehension = __import__('0-1-async_comprehension').async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
 
     start_time = time.time()
     tasks = [asyncio.create_task(async_comprehension())
-             for x in range(4)]
-    await asyncio.sleep(10)
+             for _ in range(4)]
+    await asyncio.gather(*tasks)
     end_time = time.time()
     total_time = end_time - start_time
 
