a5793043c3b62ad927fc9c419ada47917789ea85
User10538 <10538@holbertonstudents.com>
2025-07-30 18:11:35 +1000

    Fix error

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -14,8 +14,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """
-    Import async_comprehension from the previous file and 
-    write a measure_runtime coroutine that will 
+    Import async_comprehension from the previous file and
+    write a measure_runtime coroutine that will
     execute async_comprehension four times in parallel using asyncio.gather.
 
     Args:
