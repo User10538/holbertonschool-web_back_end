b045c316f1909e5c0b8ce571842834e007f5f795
User10538 <10538@holbertonstudents.com>
2025-07-30 15:58:07 +1000

    4. Tasks

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python3
 """
-Create afunction task_wait_random that takes an integer
-max_delay and returns a asyncio.Task.
+Import wait_random and write an async routine called wait_n
+ that takes in 2 int. wait_n should return the list of all
+ the delays (float values). The list of the delays
+should be in ascending order without using sort() because of concurrency.
 """
 
 
+import random
 import asyncio
-from typing import Callable
+from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-def task_wait_random(max_delay: int = 0) -> asyncio.Task:
+async def task_wait_n(n: int, max_delay: int = 0) -> List[float]:
     """
-    Create a function task_wait_random that takes an integer
-    max_delay and returns a asyncio.Task.
+    integer argument max_delay, with a default value of 10 named wait_random
+      that waits for a random delay between 0 and max_delay
 
     Args:
+    n: int
     max_delay: int = 10
 
     Returns:
-    asyncio.Task
+    list of all the delays (float values)
     """
-    return asyncio.create_task(wait_random(max_delay))
+    delays = []  # This will store the delays returned
+    tasks = []   # This array of function will store the task objects
+
+    # Get all the coroutines (not running yet)
+    #for 0 to 5 times or for 0 to 10
+    for x in range(n):
+        task = task_wait_random(max_delay)
+        # Save the task value
+        tasks.append(task)
+
+    # Process each task as it finishes
+    for finished_task in asyncio.as_completed(tasks):
+        # Wait for this specific one to finish
+        result = await finished_task
+        # Save the delay value
+        delays.append(result)
+
+    return delays
