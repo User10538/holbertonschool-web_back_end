5e4bed5b674181f6c1d23e12eae9a43a47bf5db3
User10538 <10538@holbertonstudents.com>
2025-08-11 12:44:48 +1000

    Simple helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def index_range(page: int, page_size: int) -> tuple[int,int]:
+    start_index = page - 1 * page_size
+    end_index = page * page_size
+
+    return(start_index, end_index)
