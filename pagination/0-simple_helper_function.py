622c1bd7fe50709e3e2fcff98676c0a15ddf5f96
User10538 <10538@holbertonstudents.com>
2025-08-11 12:48:35 +1000

    Fix error

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-def index_range(page: int, page_size: int) -> tuple[int,int]:
+def index_range(page: int, page_size: int) -> tuple[int, int]:
     start_index = (page - 1) * page_size
     end_index = page * page_size
 
-    return(start_index, end_index)
+    return (start_index, end_index)
